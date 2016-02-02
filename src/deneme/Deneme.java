29bcef3fcebdcf2de5bc129ff666584d3c530d1b
ALTAN ŞENEL <altan.senel@gunessigorta.com.tr>
2016-02-02 15:48:15 +0200

    PRD-HEAD ilk commit

---
+++ src/deneme/Deneme.java
@@ -0,0 +1,32 @@
+package deneme;
+
+public class Deneme {
+	
+	private String isim;
+	private Integer sayi;
+	private Boolean durum;
+	public String getIsim() {
+		return isim;
+	}
+	public void setIsim(String isim) {
+		this.isim = isim;
+	}
+	public Integer getSayi() {
+		return sayi;
+	}
+	public void setSayi(Integer sayi) {
+		this.sayi = sayi;
+	}
+	public Boolean getDurum() {
+		return durum;
+	}
+	public void setDurum(Boolean durum) {
+		this.durum = durum;
+	}
+	public String toString() {
+		return " isim: " + isim + ", sayi: " + sayi + ", durum: " + durum;
+	}
+	
+	
+
+}
